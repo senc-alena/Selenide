f64f59c742902f41b018cee1aa2264ab559d1c9e
senc-alena <sencalena1@gmail.com>
2026-02-12 01:25:37 +0300

    testCharHover is added

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -1,13 +1,58 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class HomeTest extends BaseTest {
 
-    @Test
-    public void clickFuturesBtn() {
+    @BeforeMethod
+    public void login() {
         loginPage.openPage();
         loginPage.login();
+    }
+
+    @Test(priority = 1)
+    public void checkClickFuturesBtn() {
         homePage.clickFuturesBtn();
+    }
+
+    @Test(priority = 2)
+    public void checkClickDataChoice() {
+        homePage.clickDataChoice();
+    }
+
+    @Test(priority = 3)
+    public void checkDataSide() {
+        homePage.clickDataChoice();
+        homePage.haveDataSide();
+    }
+
+    @Test(priority = 4)
+    public void selectSingleDate() {
+        homePage.clickDataChoice();
+        homePage.selectCustomDate(4, "January", 2026);
+    }
+
+    @Test(priority = 5)
+    public void selectDateRange() {
+        homePage.clickDataChoice();
+        homePage.selectCustomRange(4, "January",
+                2026, 16, "January", 2026);
+    }
+
+    @Test(priority = 6)
+    public void testChartHover() {
+
+        homePage.clickDataChoice();
+        homePage.selectCustomRange(5, "January",
+                2026, 18, "January", 2026);
+        homePage.hoverOverChart();
+        homePage.verifyTooltipAppears();
+
+        String date = homePage.getTooltipDate();
+        String value = homePage.getTooltipValue();
+
+        System.out.println("Tooltip date: " + date);
+        System.out.println("Tooltip value: " + value);
     }
 }
